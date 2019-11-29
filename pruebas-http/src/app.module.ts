ebc7950b3a778c212258ff83a28407de905ada71
JohaCR <caro.cerezor@gmail.com>
2019-11-29 16:04:21 -0500

    Conexion a la base de datos y creacion de tablas

--- pruebas-http/src/app.module.ts
+++ pruebas-http/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario/usuario.entity";
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: 'localhost',
+        port: 32775,
+        username: 'johanna',
+        password: 'johaweb',
+        database: 'web',
+        entities: [
+          UsuarioEntity
+        ],
+        synchronize: true, // Crear -> true , Conectar -> false
+      }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
